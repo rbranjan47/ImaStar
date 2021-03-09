7031fd8d0f4e005cba7bc343fb73bdbae02ab9fd
Thinksysuser <Thinksysuser@10.101.23.91>
2021-03-09 12:56:04 +0530

    Commit

---
+++ iamstar_automation/src/main/java/imastar/iamstar_automation/App.java
@@ -0,0 +1,13 @@
+package imastar.iamstar_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
